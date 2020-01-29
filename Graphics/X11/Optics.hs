2205ce3cf8e40b687cc3393f4f2a54232ebbab90
wygulmage <wygulmage@users.noreply.github.com>
2020-01-29 08:57:16 -0500

    changed (^.) and (.~) to view and set, removed (%~).

--- Graphics/X11/Optics.hs
+++ Graphics/X11/Optics.hs
@@ -17,27 +17,20 @@ import Graphics.X11.Xlib.Types
 type Mono t a b = t a a b b
 type Lens ta tb a b = forall m. Functor m => (a -> m b) -> ta -> m tb
 -- Lens laws:
--- (o .~ v $ s) ^. o = v
--- o .~ (s ^. o) s = s
--- (o .~ v') . (o .~ v) = o .~ v'
+-- view o (set o v s) = v
+-- set o (view o s) s = s
+-- set o v' . set o v = set o v'
 
-(^.) :: ta -> ((a -> Const a a) -> ta -> Const a ta) -> a
-s ^. o = getConst (o Const s)
-infixl 8 ^.
+view :: ((a -> Const a a) -> ta -> Const a ta) -> ta -> a
+view o s = getConst (o Const s)
 
-(%~) :: ((a -> Identity b) -> ta -> Identity tb) -> (a -> b) -> ta -> tb
--- o %~ f = runIdentity . o (Identity . f)
-o %~ f = coerce (o (coerce f))
-infixr 4 %~
-
-(.~) :: ((a -> Identity b) -> ta -> Identity tb) -> b -> ta -> tb
-o .~ a = o %~ const a
-infixr 4 .~
-
+set :: ((a -> Identity b) -> ta -> Identity tb) -> b -> ta -> tb
+set o a = runIdentity . o (Identity . const a)
+infixr 4 `set`
 
 -- Has- classes --
 
-  -- HasRectangle could be a superclass of HasSegment, with the Rectangle as the bounding rectangle of the segment. But this could lead to confusion when, eg., the 'height' of an arc is height of the bounding rectangle of the enpoints of the arc rather than that of the arc.
+  -- HasRectangle could be a superclass of HasSegment, with the Rectangle as the bounding rectangle of the segment. But this could lead to confusion when, eg., the 'height' of an arc is height of the bounding rectangle of the endpoints of the arc rather than that of the arc.
   -- Reflecting the diagonal of a rectangle by changing x2 or y2 also changes x1 or y2.
 
 -- | HasPoint: Has a point in the (x,y) coordinate plane.
@@ -45,14 +38,14 @@ infixr 4 .~
 class HasPoint a where
     _Point :: Mono Lens a Point
     _Point f s =
-      (\ (Point x y) -> (_x.~ x) . (_y.~ y) $ s)
-      <$> f (Point <$> (^._x) <*> (^._y) $ s)
+      (\ (Point x y) -> set _x x . set _y y $ s)
+      <$> f (Point <$> view _x <*> view _y $ s)
 
     _x :: Mono Lens a Position
-    _x = _Point._x
+    _x = _Point . _x
 
     _y :: Mono Lens a Position
-    _y = _Point._y
+    _y = _Point . _y
 
 instance HasPoint Point where
     _Point = id
@@ -79,8 +72,8 @@ class HasPoint a => HasSegment a where
     _Segment :: Mono Lens a Segment
     _Segment f s =
         (\ (Segment x1 y1 x2 y2) ->
-           (_x.~ x1) . (_y.~ y1) . (_x2.~ x2) . (_y2 .~ y2) $ s)
-        <$> f (Segment <$> (^._x) <*> (^._y) <*> (^._x2) <*> (^._y2) $ s)
+           set _x x1 . set _y y1 . set _x2 x2 . set _y2 y2 $ s)
+        <$> f (Segment <$> view _x <*> view _y <*> view _x2 <*> view _y2 $ s)
 
     _Point2 :: Mono Lens a Point
     _Point2 = _Segment . _Point2
@@ -99,44 +92,13 @@ instance HasSegment Segment where
     _x2 f s = (\ x2 -> s{ seg_x2 = x2 }) <$> f (seg_x2 s)
     _y2 f s = (\ y2 -> s{ seg_y2 = y2 }) <$> f (seg_y2 s)
 
-instance HasSegment Rectangle where
-    -- The segment is the rectangle's diagonal from upper left to lower right.
-    _Segment f = fmap diagonalToRectangle . f . rectangleToDiagonal
-    _x2 f (Rectangle x y w h) =
-        (\ (x', w') -> Rectangle x' y w' h) . toDisplacement x
-        <$> f (x + fromIntegral w)
-    _y2 f (Rectangle x y w h) =
-        (\ (y', h') -> Rectangle x y' w h') . toDisplacement y
-        <$> f (y + fromIntegral h)
-
-rectangleToDiagonal :: Rectangle -> Segment
-rectangleToDiagonal (Rectangle x y w h) =
-  Segment x y (x + fromIntegral w) (y + fromIntegral h)
-
-diagonalToRectangle :: Segment -> Rectangle
-diagonalToRectangle (Segment x1 y1 x2 y2) =
-    Rectangle x y w h
-      where
-        (x, w) = toDisplacement x1 x2
-        (y, h) = toDisplacement y1 y2
-
-fromDisplacement :: Position -> Dimension -> (Position, Position)
--- ^ This should be safe for X, because the Haskell numbers have a larger range than the X version, but in general the displacement form has a larger range.
-fromDisplacement x w = (x, x + fromIntegral w)
-
-toDisplacement :: Position -> Position -> (Position, Dimension)
-toDisplacement x1 x2
-  | x1 < x2 = (x1, fromIntegral (x2 - x1))
-  | otherwise = (x2, fromIntegral (x1 - x2))
-
-
 -- | HasDimensions: Has a width and a height.
 
 class HasDimensions a where
     _Dimensions :: Mono Lens a Dimensions
     _Dimensions f s =
-        (\ (Dimensions w h) -> (_width .~ w) . (_height .~ h) $ s)
-        <$> f (Dimensions <$> (^._width) <*> (^._height) $ s)
+        (\ (Dimensions w h) -> set _width w . set _height h $ s)
+        <$> f (Dimensions <$> view _width <*> view _height $ s)
 
     _width :: Mono Lens a Dimension
     _width = _Dimensions . _width
@@ -166,3 +128,23 @@ instance HasDimensions Arc where
  --        (x, w) = toDisplacement x1 x2
  --        denormalize w'
  --          | x1 <= x2 = Segment x1 y1 (fromIntegral w' - x1)
+
+rectangleToDiagonal :: Rectangle -> Segment
+rectangleToDiagonal (Rectangle x y w h) =
+  Segment x y (x + fromIntegral w) (y + fromIntegral h)
+
+diagonalToRectangle :: Segment -> Rectangle
+diagonalToRectangle (Segment x1 y1 x2 y2) =
+    Rectangle x y w h
+      where
+        (x, w) = toDisplacement x1 x2
+        (y, h) = toDisplacement y1 y2
+
+fromDisplacement :: Position -> Dimension -> (Position, Position)
+-- ^ This should be safe for X, because the Haskell numbers have a larger range than the X version, but in general the displacement form has a larger range.
+fromDisplacement x w = (x, x + fromIntegral w)
+
+toDisplacement :: Position -> Position -> (Position, Dimension)
+toDisplacement x1 x2
+  | x1 < x2 = (x1, fromIntegral (x2 - x1))
+  | otherwise = (x2, fromIntegral (x1 - x2))
